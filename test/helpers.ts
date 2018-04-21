76f69f996d42c4aff7af86ee6a50be1afc5ea875
Heather Roberts <heather.roberts@3plearning.com>
2018-04-21 19:26:01 +1000

    Comment TestHelpers class

--- test/helpers.ts
+++ test/helpers.ts
@@ -3,6 +3,11 @@ import * as fs from "fs";
 import * as path from "path";
 
 export class TestHelpers {
+
+  /**
+   * Runs tslint on the fixture with only the lines-between-class-members rule enabled
+   * Returns the linting results
+   */
   static lint(fixture: string): LintResult {
     const linter = new Linter({ formatter: 'json', fix: false });
 
